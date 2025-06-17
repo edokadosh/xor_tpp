0b1d711126c067f2e443d77096a1a2e52388e83e
edokadosh <edotiki@gmail.com>
2025-06-17 13:56:32 +0300

    done optimus

--- xor_tpp/optimus.cpp
+++ xor_tpp/optimus.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
-using std::string;
-using std::to_string;
 
-int main()
-{
-
-    for (int i = 1; i <= 11; i++)
-    {
-        for (int j = 1; j <= 11; j++)
-        {
-            cout << i * j << string((4 - to_string(i * j).length()), ' ');
+// Check if unsigned value is prime
+bool isPrime(unsigned num) {
+    for (int i = 2; i * i < num; i++) {
+        if (num % i == 0) {
+            return false;
         }
-        cout << endl;
+    }
+    return true;
+}
+
+int main() {
+    std::vector<unsigned> numbersToCheck = {5, 23, 237, 7};
+
+    for (auto num : numbersToCheck) {
+        cout << num << " is " << (isPrime(num) ? " " : " not ") << "prime" << endl;
     }
 
     return 0;
