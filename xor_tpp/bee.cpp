03efe55eaa8a04d8dd08c3ed50195507cf546a66
edokadosh <edotiki@gmail.com>
2025-06-17 16:46:53 +0300

    changed bumblebee to raii

--- xor_tpp/bee.cpp
+++ xor_tpp/bee.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <string>
-#include <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
-using std::vector;
 
 constexpr unsigned NUMBER_OF_PRIMES_TO_FIND = 10;
 
@@ -20,7 +17,7 @@ bool isPrime(unsigned num) {
     return true;
 }
 
-void findNumberOfFirstPrimes(unsigned* result, unsigned numberOfPrimesToFind) {
+void findNumberOfFirstPrimes(std::unique_ptr<unsigned[]> &result, unsigned numberOfPrimesToFind) {
     unsigned currentNumber = 2;
     unsigned index = 0;
 
@@ -33,7 +30,7 @@ void findNumberOfFirstPrimes(unsigned* result, unsigned numberOfPrimesToFind) {
     }
 }
 
-void printArray(const unsigned *arr, unsigned length) {
+void printArray(std::unique_ptr<unsigned[]>  &arr, unsigned length) {
     for (unsigned i = 0; i < length; i++) {
         cout << arr[i] << endl;       
     }
@@ -44,14 +41,12 @@ void printArray(const unsigned *arr, unsigned length) {
 
 
 int main() {
-    unsigned *result = new unsigned[NUMBER_OF_PRIMES_TO_FIND];
+    std::unique_ptr<unsigned[]> result(new unsigned[NUMBER_OF_PRIMES_TO_FIND]);
 
     findNumberOfFirstPrimes(result, NUMBER_OF_PRIMES_TO_FIND);
 
     cout << "Got primes:" << endl;
     printArray(result, NUMBER_OF_PRIMES_TO_FIND);
-
-    delete[] result;
 
     return 0;
 }
