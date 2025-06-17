4761e3ba97a37d3114139bc9ff84a518592776c9
edokadosh <edotiki@gmail.com>
2025-06-17 11:40:14 +0300

    fix groot

--- xor_tpp/groot.cpp
+++ xor_tpp/groot.cpp
@@ -3,14 +3,19 @@
 using std::cin;
 using std::cout;
 using std::endl;
+using std::sqrt;
 
-int main()
-{
-    int number;
+int main() {
+    int number = 0;
 
     cout << "Enter number: ";
     cin >> number;
-    cout << "The square root is: " << std::sqrt(number) << endl;
+    if (cin.fail())
+    {
+        cout << "Expected integer" << endl;
+        return 0;
+    }
+    cout << "The square root is: " << sqrt(number) << endl;
 
     return 0;
 }
