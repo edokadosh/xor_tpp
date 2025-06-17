5be534786cac0f320595aa47249fb8c60ca70d9f
edokadosh <edotiki@gmail.com>
2025-06-17 14:18:20 +0300

    fix fold

--- xor_tpp/fold.cpp
+++ xor_tpp/fold.cpp
@@ -4,30 +4,32 @@
 using std::cin;
 using std::cout;
 using std::endl;
+
+constexpr unsigned TABLE_PADDING = 4;
+
+namespace {
+
 using std::string;
 using std::to_string;
 
-#define TABLE_PADDING 4
-
-// pad integert 
+// right pad a number with spaces
 string padInt(int num, int paddingLength) {
-    return string((paddingLength - to_string(num).length()), ' ');
+    return string((paddingLength - to_string(num).length()), ' ') + to_string(num);
 }
 
 // print multiplication chart
 void printMult() {
-    for (int i = 1; i <= 11; i++)
-    {
-        for (int j = 1; j <= 11; j++)
-        {
-            cout << i * j << padInt(i * j, TABLE_PADDING);
+    for (int i = 1; i <= 11; i++) {
+        for (int j = 1; j <= 11; j++) {
+            cout << padInt(i * j, TABLE_PADDING);
         }
         cout << endl;
     }
 }
 
+} // namespace
+
 int main() {
     printMult();
-
     return 0;
 }
