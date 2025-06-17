e93b9a1d041e472fb8b7372f1c0bdb58d38bbfc2
edokadosh <edotiki@gmail.com>
2025-06-17 12:15:27 +0300

    fix

--- xor_tpp/groot.cpp
+++ xor_tpp/groot.cpp
@@ -6,14 +6,19 @@ using std::endl;
 using std::sqrt;
 
 int main() {
-    int number = 0;
+    double number = 0;
 
     cout << "Enter number: ";
     cin >> number;
     if (cin.fail())
     {
-        cout << "Expected integer" << endl;
-        return 0;
+        cout << "Expected double" << endl;
+        return 1;
+    }
+    if (number < 0)
+    {
+        cout << "Expected positive value" << endl;
+        return 1;
     }
     cout << "The square root is: " << sqrt(number) << endl;
 
