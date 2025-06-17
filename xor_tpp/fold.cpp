35f958bd7d526aa1a4f0b22e96810781623a534e
edokadosh <edotiki@gmail.com>
2025-06-17 11:39:27 +0300

    fix fold

--- xor_tpp/fold.cpp
+++ xor_tpp/fold.cpp
@@ -4,14 +4,17 @@
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
+using std::to_string;
 
-int main()
-{
+
+int main() {
+    
     for (int i = 1; i <= 11; i++)
     {
         for (int j = 1; j <= 11; j++)
         {
-            cout << i * j << std::string((4 - std::to_string(i * j).length()), ' ');
+            cout << i * j << string((4 - to_string(i * j).length()), ' ');
         }
         cout << endl;
     }
