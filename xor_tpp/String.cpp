7125caec4398abd6a8959a733293a0e6dead886d
edokadosh <edotiki@gmail.com>
2025-06-18 10:13:12 +0300

    fixed string

--- xor_tpp/String.cpp
+++ xor_tpp/String.cpp
@@ -2,18 +2,18 @@
 
 
 String::String(const char *str) {
-    length = strlen(str);
-    chars = std::make_unique<char[]>(strlen(str)+1);
-    unsigned index = 0;
+    m_length = strlen(str);
+    m_chars = std::make_unique<char[]>(m_length + 1);
+    size_t index = 0;
     do {
-        chars[index] = str[index];
+        m_chars[index] = str[index];
     } while (str[index++]);
 }
 
 
 std::ostream &operator<<(std::ostream &os, const String &obj) {
-    for (int i = 0; i < obj.length; i++) {
-        os << obj.chars[i];
+    for (int i = 0; i < obj.m_length; i++) {
+        os << obj.m_chars[i];
     }
     return os;
 }
--- xor_tpp/String.h
+++ xor_tpp/String.h
@@ -1,14 +1,13 @@
 #pragma once
 #include <iostream>
 
-
 class String {
-  private:
-    std::unique_ptr<char[]> chars = nullptr;
-    size_t length;
-
   public:
-    String(const char* str);
+    String(const char *str);
 
     friend std::ostream &operator<<(std::ostream &os, const String &obj);
+
+  private:
+    std::unique_ptr<char[]> m_chars = nullptr;
+    size_t m_length;
 };
