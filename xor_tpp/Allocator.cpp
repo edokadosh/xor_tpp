09ed700e9e34b46fdc3568c3c56e9fc190ec2c7a
edokadosh <edotiki@gmail.com>
2025-06-18 14:49:06 +0300

    wip miktze

--- xor_tpp/Allocator.cpp
+++ xor_tpp/Allocator.cpp
@@ -1,12 +1,49 @@
 #include "Allocator.h"
+#include <stdlib.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 namespace allocator {
 
-void s_AllocationNode::remove();
+void AllocationNode::removeSelf() {
+    if (prev) {
+        prev->next = next;
+    } else {
+        first = next;
+    }
+    if (next) {
+        next->prev = prev;
+    }
+}
 
-void* operator new(size_t size);
+void* operator new(size_t size) {
+    char* newMem = (char*)malloc(sizeof(AllocationNode) + size);
+    AllocationNode* newNode = (AllocationNode*)newMem;
+    newNode->size = size;
+    newNode->prev = nullptr;
+    newNode->next = first;
+    if (first) {
+        first->prev = newNode;
+    }
+    first = newNode;
 
-void operator delete(void* ptr);
+    return (newMem + sizeof(AllocationNode));
+}
 
-void printAllocations();
+void operator delete(void* ptr) {
+    AllocationNode* nodeToDelete = (AllocationNode*)((char*)ptr - sizeof(AllocationNode));
+    nodeToDelete->removeSelf();
+    free(nodeToDelete);
+}
+
+void printAllocations() {
+    AllocationNode* curr = first;
+    cout << (curr ? "Memory Allocations" : "No memory allocated") << endl;
+    while (curr) {
+        cout << curr + 1 << ": Memory allocation of size - " << curr->size << endl; 
+        curr = curr->next;
+    }
+}
 } // namespace allocator
--- xor_tpp/miktze.cpp
+++ xor_tpp/miktze.cpp
@@ -1,3 +1,19 @@
 #include "Allocator.h"
+#include <iostream>
 
+using std::cout;
+using std::endl;
+using allocator::operator new;
+using allocator::operator delete;
+using allocator::printAllocations;
+
+
+int main() {
+    void* a = new int[6];
+    void* b = new char[10];
+    printAllocations();
+    delete a;
+    delete b;
+    printAllocations();
+}
 
