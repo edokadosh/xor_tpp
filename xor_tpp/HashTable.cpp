1ae8ddc32cfcca2c1ace7f35af418ab238f7bfb4
edokadosh <edotiki@gmail.com>
2025-06-19 10:17:56 +0300

    --msg

--- xor_tpp/HashTable.cpp
+++ xor_tpp/HashTable.cpp
@@ -1,1 +1,97 @@
 #include "HashTable.h"
+#include <utility>
+
+using std::exchange;
+using std::swap;
+using std::hash;
+
+namespace hash_table {
+
+template <typename V>
+HashTable<V>::HashTable(size_t tableSize /*= DEFAULT_HASH_TABLE_SIZE*/)
+    : m_tableSize{tableSize}, m_items{new pair<string, V>*[tableSize]} {
+    for (size_t i = 0; i < tableSize; i++) {
+        m_items[i] = nullptr;
+    }
+}
+
+template <typename V> HashTable<V>::~HashTable() {
+    for (size_t i = 0; i < tableSize; i++) {
+        delete m_items[i];
+    }
+    delete[] m_items;
+}
+
+template <typename V>
+HashTable<V>::HashTable(const HashTable& other)
+    : m_tableSize{other.m_tableSize}, m_items{new pair<string, V>*[other.m_tableSize]} {
+
+    for (size_t i = 0; i < tableSize; i++) {
+        if (other.m_items[i]) {
+            m_items[i] = new pair<string, V>{*other.m_items[i]};
+        }
+    }
+}
+
+template <typename V>
+HashTable<V>::HashTable(HashTable&& other)
+    : m_tableSize{exchange(other.m_tableSize, 0)}, m_items{exchange(other.m_items, nullptr)} {
+    // Left blank intentionally
+}
+
+template <typename V> HashTable<V>& HashTable<V>::operator=(const HashTable& other) {
+    return *this = HashTable<V>(other);
+}
+
+template <typename V> HashTable<V>& HashTable<V>::operator=(HashTable<V>&& other) noexcept {
+    swap(m_tableSize, other.m_tableSize);
+    swap(m_items, other.m_items);
+    return *this;
+
+}
+
+template <typename V> V& HashTable<V>::operator[](const string& key) {
+    int keyHash = hash<string>{}(key) % m_tableSize;
+
+    if (!m_items[keyHash]) {
+        *m_items[keyHash] = new pair<string, V>;
+    } 
+    return *m_items[keyHash];
+}
+
+template <typename V> V HashTable<V>::operator[](const string& key) const {
+    if (!m_items[keyHash]) {
+        return *m_items[keyHash];
+    } else {
+        return pair<string, V>{};
+    }
+}
+
+template <typename V> void HashTable<V>::remove(const string& key) {
+    int keyHash = hash<string>{}(key) % m_tableSize;
+    delete m_items[keyHash];
+}
+
+template <typename V> size_t HashTable<V>::size() const {
+    size_t size = 0;
+    for (size_t i = 0; i < tableSize; i++) {
+        if (m_items[i]) {
+            size++;
+        }
+    }
+    return size;
+}
+
+template <typename V> string HashTable<V>::find(const V& value) const {
+    for (size_t i = 0; i < tableSize; i++) {
+        if (m_items[i] and m_items[i].second == value) {
+            return m_items[i].first;
+        }
+    }
+    return "";
+}
+
+template <typename V> bool HashTable<V>::hasKey(const string& key) const {
+}
+
+} // namespace hash_table
--- xor_tpp/HashTable.h
+++ xor_tpp/HashTable.h
@@ -1,2 +1,36 @@
 #pragma once
-class HashTable {};
+#include <string>
+#include <utility>
+
+using std::pair;
+using std::string;
+
+namespace hash_table {
+
+constexpr size_t DEFAULT_HASH_TABLE_SIZE = 100;
+const char* NOT_FOUND = "";
+
+template <typename V> class HashTable {
+  public:
+    HashTable(size_t tableSize = DEFAULT_HASH_TABLE_SIZE);
+
+    virtual ~HashTable();
+    HashTable(const HashTable& other);
+    HashTable(HashTable&& other) noexcept;
+    HashTable& operator=(const HashTable& other);
+    HashTable& operator=(HashTable&& other) noexcept;
+
+    V& operator[](const string& key);
+    V operator[](const string& key) const;
+
+    void remove(const string& key);
+    size_t size() const;
+    string find(const V& value) const;
+    bool hasKey(const string& key) const;
+
+  private:
+    pair<string, V>** m_items;
+    size_t m_tableSize;
+};
+
+} // namespace hash_table
